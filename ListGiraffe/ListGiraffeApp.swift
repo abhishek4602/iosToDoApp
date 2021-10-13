13478f898abd502472fcdc202f3c5e319c41179d
Abhishek Bagdare <bagdare.a@northeastern.edu>
2021-10-13 18:50:45 -0400

    Initial Commit

---
+++ ListGiraffe/ListGiraffeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListGiraffeApp.swift
+//  ListGiraffe
+//
+//  Created by Abhishek Bagdare on 10/13/21.
+//
+
+import SwiftUI
+
+@main
+struct ListGiraffeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
